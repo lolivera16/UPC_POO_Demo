3e7cf762ba56d9050f1460acb6ed8e77832000db
Your Name <roosevelt.0905@gmail.com>
2012-07-12 19:52:27 -0500

    Esta es mi prueba

---
+++ LuisOlivera.java
@@ -0,0 +1,12 @@
+/**
+  |---------------------------------------------------------------
+  |  LuisOlivera.java
+  |---------------------------------------------------------------
+  | @Autor: Luis Olivera Aguilar
+  | @Fecha de creación: 12/07/2012
+  | @Modificado por: Luis OLivera
+  | @Ultima fecha de modificacion: 09/07/2012
+  | @Grupo: News
+  |---------------------------------------------------------------
+  | Archivo donde se ejecuta tal cosa ......
+ */
